3c18bf29ec48f1e0ca39c4ae14fca3425dfad502
adhil <162969676+adhil213@users.noreply.github.com>
2026-08-21 08:12:59 +0300

    Time: 11 ms (78.61%) | Memory: 61.2 MB (7.36%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    let freq = new Map();
+
+    for (let char of s) {
+        freq.set(char, (freq.get(char) || 0) + 1);
+    }
+
+    for (let char of t) {
+        if (!freq.has(char)) {
+            return false;
+        }
+
+        freq.set(char, freq.get(char) - 1);
+
+        if (freq.get(char) < 0) {
+            return false;
+        }
+    }
+
+    return true;
+};
